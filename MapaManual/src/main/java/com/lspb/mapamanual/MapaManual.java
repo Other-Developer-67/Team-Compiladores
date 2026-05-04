cf8723c9fa718cbbaa473b11b79027935e7abab9
Other-Developer-67 <dforooficial@gmail.com>
2026-05-04 16:29:21 -0600

    Visor de Mapas (Con FlatLaf)
    Programa que visualiza mapas de manera virtual con jxmapviewer2 y busqueda automatica de lugares conectandose a  nominatim con json, version con integracion de FlatLaf

---
+++ MapaManual/src/main/java/com/lspb/mapamanual/MapaManual.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.lspb.mapamanual;
+
+
+/**
+ *
+ * @author santiago
+ */
+public class MapaManual {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        FramePrincipal MainFrame = new FramePrincipal();
+        MainFrame.setVisible(true);
+    }
+}
